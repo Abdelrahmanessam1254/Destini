2db5491da728509d2f32760fb0d62e817db9d004
Abdelrahmanessam1254 <abdelrahmanessam007@gmail.com>
2023-09-14 23:33:44 +0300

    first Commit

---
+++ lib/story.dart
@@ -0,0 +1,13 @@
+class Story {
+  final String storyTitle;
+  final String choice1;
+  final String choice2;
+
+  Story({
+    required this.storyTitle,
+    required this.choice1,
+    required this.choice2,
+  });
+
+// Add any additional methods or properties here, if needed.
+}
